8986328c05d2b2a9867c3f112e70159cf814f3af
unknown <hjorturjohann94@gmail.com>
2019-03-26 14:11:40 +0000

    stuff

--- Assignment3/smallassignment3/src/components/navigationBar/navigationBar.js
+++ Assignment3/smallassignment3/src/components/navigationBar/navigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavLinks from '../NavLinks/NavLinks';
 
+
 const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-lg  navigation-bar">
