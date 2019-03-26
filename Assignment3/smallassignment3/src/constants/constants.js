10f9e89e70782528a30cec956f9192091a75baf0
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-03-26 14:21:52 +0000

    constants

--- Assignment3/smallassignment3/src/constants/constants.js
+++ Assignment3/smallassignment3/src/constants/constants.js
@@ -1,1 +1,5 @@
 export const GET_ALL_BOSSES = 'GET_ALL_BOSSES';
+export const GET_BOSS_BY_ID = 'GET_BOSS_BY_ID';
+export const PATCH_BOSS_BY_ID = 'PATCH_BOSS_BY_ID';
+export const POST_BOSS_BY_ID = 'POST_BOSS_BY_ID';
+export const DELETE_BOSS_BY_ID = 'DELETE_BOSS_BY_ID';
