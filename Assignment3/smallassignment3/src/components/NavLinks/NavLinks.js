717f8a2e84954c1d4c8ff26c70f2d342d155630a
gudrunivans <gudruni17@ru.is>
2019-03-26 17:09:30 +0000

    navigation bar

--- Assignment3/smallassignment3/src/components/NavLinks/NavLinks.js
+++ Assignment3/smallassignment3/src/components/NavLinks/NavLinks.js
@@ -4,12 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavLinks = () => {
     return (
     <>
-     
         <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
                 <NavLink  className="nav-link" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item active">
                 <NavLink className="nav-link" to="/bosses">Bosses</NavLink>
             </li>
         </ul>
