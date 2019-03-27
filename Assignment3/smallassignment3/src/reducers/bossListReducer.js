aff9b342714e8e0b14fccc645ff4bfffdfc0f09b
HjorturJ <hjorturv17@ru.is>
2019-03-27 00:34:31 +0000

    done

--- Assignment3/smallassignment3/src/reducers/bossListReducer.js
+++ Assignment3/smallassignment3/src/reducers/bossListReducer.js
@@ -4,7 +4,7 @@ export default function(state = [], action) {
   switch (action.type) {
     case CST.GET_ALL_BOSSES: return action.payload;
     case CST.POST_BOSS_BY_ID: return [...state, { id: action.payload.id, ...action.payload.newBossDetails }];
-    case CST.DELETE_BOSS_BY_ID: return state.filter(boss => boss.id !== action.payload);  
+    case CST.DELETE_BOSS_BY_ID: return state.filter(boss => boss.id !== parseInt(action.payload));
     default: return state;
   };
 };
